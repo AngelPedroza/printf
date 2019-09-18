203d85d1b36be05c65347095af6e9a9e36b8da87
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-18 17:32:06 +0000

    added description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main(void) /* outputs the expected string*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

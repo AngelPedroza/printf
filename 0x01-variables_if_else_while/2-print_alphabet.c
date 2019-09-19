67d6672ae980ceef7f0ee996a9763ae053666d9a
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-19 17:41:06 +0000

    added source for task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+*main - prints the alphabet in lowercase
+*Return: 0
+*/
+int main(void)
+{
+	int ch = 'a';
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}

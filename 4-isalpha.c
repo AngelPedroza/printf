086362c63a7fea5a40ed6c559d6bb6bd5b197d28
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-23 22:23:25 +0000

    test

---
+++ 4-isalpha.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+
+/**
+ * _islower - checks for lowercase char
+ * @c: the character to check
+ * Return: On lowercase 1
+ * Otherwise 0.
+ */
+
+
+int _islower(int c)
+{
+        int lastcase = 123;
+        int firstcase = 97;
+        if ((c >= firstcase) && (c <= lastcase))
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+    
+	}
---
+++ holberton.h~
@@ -0,0 +1,9 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+
+#endif

3f22800f054efc01f84e5c25f47b26b274022b6e
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-24 22:49:02 +0000

    reqs for task 2

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -6,3 +6,11 @@
 void positive_or_negative(int i);
 
 #endif /* HOLBERTON_H */
+
+#ifndef LARGEST_NUMBER
+#define LARGEST_NUMBER
+
+
+int largest_number(int a, int b, int c);
+
+#endif /* HOLBERTON_H */

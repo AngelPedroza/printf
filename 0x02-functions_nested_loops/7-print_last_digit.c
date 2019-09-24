2ec2f9ab6de1cd7856f8357a5876ac134e5eb25f
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-24 01:33:59 +0000

    to continue on home

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+
+/**
+ * print_last_digit - returns last digit of a number
+ * @n: the number to check
+ * Return: number
+ */
+
+
+int print_last_digit(int n)
+{
+	int retval = n % 10;
+
+	return (retval);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+
+/**
+ * print_alphabet_x10 - writes alphabet 10 times
+ *
+ *
+ * Return: none
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void jack_bauer(void)
+{
+	int i = 0;
+
+	while (i < 23)
+	{
+		int j = 0;
+
+		while (j < 60)
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar()
+		_putchar('\n');
+		i++;
+	}
+}

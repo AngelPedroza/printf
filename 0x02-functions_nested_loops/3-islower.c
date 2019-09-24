eb6f859909b9ba6e6f73ba52d74a8b13868069d2
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-24 14:11:36 +0000

    fix: betty format

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,9 @@
 /**
  * _islower - checks for lowercase char
  * @c: the character to check
+ *
  * Return: On lowercase 1
+ *
  * Otherwise 0.
  */
 
@@ -13,6 +15,7 @@ int _islower(int c)
 {
 	int lastcase = 123;
 	int firstcase = 97;
+
 	if ((c >= firstcase) && (c <= lastcase))
 	{
 		return (1);

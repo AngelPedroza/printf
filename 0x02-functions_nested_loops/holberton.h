5e4286a6d4fda69826044a20c083b3d31ebf9cec
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-24 13:56:52 +0000

    corrected header library

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,13 +1,57 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef _PUTCHAR
+#define _PUTCHAR
 
 int _putchar(char c);
+
+#endif
+
+
+#ifndef PRINT_ALPHABET
+#define PRINT_ALPHABET
+
 void print_alphabet(void);
+
+#endif
+
+#ifndef PRINT_ALPHABET_X10
+#define PRINT_ALPHABET_X10
+
 void print_alphabet_x10(void);
+
+#endif
+
+#ifndef _ISLOWER
+#define _ISLOWER
+
 int _islower(int c);
+
+#endif
+
+#ifndef _ISALPHA
+#define _ISALPHA
+
 int _isalpha(int c);
+
+#endif
+
+#ifndef PRINT_SIGN
+#define PRINT_SIGN
+
 int print_sign(int n);
+
+#endif
+
+#ifndef _ABS
+#define _ABS
+
 int _abs(int);
+
+#endif
+
+#ifndef PRINT_LAST_DIGIT
+#define PRINT_LAST_DIGIT
+
 int print_last_digit(int);
 
 #endif
+

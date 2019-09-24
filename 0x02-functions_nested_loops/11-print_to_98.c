31e39c0069399b5c06d46a8252efa1c62886d5a3
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-24 17:01:53 +0000

    first attempt for task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+
+/**
+ * print_to_98 - returns numbers fwd or bckwd to 98.
+ * @n: the number to check
+ *
+ */
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n < 98)
+		{
+			printf("%d ", n);
+			n++;
+		}
+		else if (n > 98)
+		{
+			printf("%d ", n);
+			n--;
+		}
+	}
+	printf("98\n");
+}

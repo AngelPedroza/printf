258c0246a9513dc3db0a039f9c75fdd1221b2137
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-24 17:03:49 +0000

    added comma to printf

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,12 +14,12 @@ void print_to_98(int n)
 	{
 		if (n < 98)
 		{
-			printf("%d ", n);
+			printf("%d, ", n);
 			n++;
 		}
 		else if (n > 98)
 		{
-			printf("%d ", n);
+			printf("%d, ", n);
 			n--;
 		}
 	}

923becb7f0602f939d33b49351fd65b32b77e124
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-26 20:50:07 +0000

    fix: starting pt of iteration

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,16 +11,17 @@ void print_line(int n)
 {
 	int i;
 
-	if (!(n <= 0))
+	if (n <= 0)
 	{
-		for (i = 0; i <= n; i++)
-		{
-			_putchar('_');
-		}
 		_putchar('\n');
 	}
 	else
 	{
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
 		_putchar('\n');
 	}
 }

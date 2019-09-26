e94e032cb1bc801367f98cedcc4e6599e12dcf39
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-26 19:48:05 +0000

    first attempt

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
 /**
- * _isupper - checks for uppercase char
- * @c: the character to check
+ * mul - multiplies 2 ints
+ * @a: multiplicando
+ * @b: multiplicador
+ * Return: result
  *
- * Return: On upper 1
  *
- * Otherwise 0.
  */
 
 
 int mul(int a, int b)
 {
 	int result = a * b;
-	return()
+
+	return (result);
 }

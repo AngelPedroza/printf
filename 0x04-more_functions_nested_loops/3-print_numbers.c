e9a8416321f54c6bcb04291af5fb9f46520a6746
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-26 19:50:47 +0000

    first attempt

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,14 @@
 
 /**
  * print_numbers - print numbers from 0 to 9
- * @c: the character to check
  *
- * Return: On upper 1
- *
- * Otherwise 0.
  */
 
 
 void print_numbers(void)
 {
 	int i;
+
 	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);

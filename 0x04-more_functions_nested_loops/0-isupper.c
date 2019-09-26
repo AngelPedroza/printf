20b27408b27ab94f1f033b37591d9dfe4eb4cc8c
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-26 15:22:43 +0000

    header and task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _isupper - checks for uppercase char
+ * @c: the character to check
+ *
+ * Return: On upper 1
+ *
+ * Otherwise 0.
+ */
+
+
+int _isupper(int c)
+{
+	int lastcap = 90;
+	int firstcap = 65;
+
+	if ((c >= firstcap) && (c <= lastcap))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

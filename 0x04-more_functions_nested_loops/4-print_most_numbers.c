dfe6efaa2107c026f9a57ea4641e9b12b8b08b47
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-26 19:56:51 +0000

    first attempt

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,18 @@
 #include "holberton.h"
 /**
  * print_most_numbers - print 0 to 9 except 2 and 4
- * @c: the character to check
  *
- * Return: On upper 1
- *
- * Otherwise 0.
  */
-
 
 void print_most_numbers(void)
 {
 	int i;
-	for( i = '0'; i <= '9'; i++)
+
+	for (i = '0'; i <= '9'; i++)
 	{
-		if(i == '2' || i == '4')
+		if (i == '2' || i == '4')
 			continue;
-		putchar(i);
+		_putchar(i);
 	}
+	_putchar('\n');
 }

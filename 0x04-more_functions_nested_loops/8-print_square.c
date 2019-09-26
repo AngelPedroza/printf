4e8f8eae886acb5e67f248b3f479da236630bb15
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-26 21:07:07 +0000

    first attempt

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,21 @@
 #include "holberton.h"
+
+/**
+ * print_square - prints square area with #
+ * @n: the size
+ *
+ */
+
+void print_square(int n)
+{
+	int l, c;
+
+	for (l = 0; l < n; l++)
+	{
+		for (c = 1; c <= n; c++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}

dc183b62b7a32f34719c968a535afcbd943ec031
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-26 22:37:10 +0000

    fix: size

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,15 +1,27 @@
 #include "holberton.h"
 
+/**
+ * print_diagonal - print a n size diagonal
+ * @n: number of diagonals
+ *
+ */
+
 void print_diagonal(int n)
 {
-        int i;
-        for (i = 0; i <= n; i++)
-        {
-                for (j = 0; j <= n; j++)
-                {
-                        _putchar(92);
-                        _putchar('\n');
-                }
-        }
-	_putchar('\n');
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
 }

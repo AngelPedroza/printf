67f902d59bd611b6968d49f3262ed3240a410d41
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-26 19:02:01 +0000

    fix: header library

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -6,13 +6,81 @@ int _putchar(char c);
 #endif
 
 
+#ifndef _ISUPPER
+#define _ISUPPER
+
 int _isupper(int c);
+
+#endif
+
+
+#ifndef _ISDIGIT
+#define _ISDIGIT
+
 int _isdigit(int c);
+
+#endif
+
+
+#ifndef MUL
+#define MUL
+
 int mul(int a, int b);
+
+#endif
+
+
+#ifndef PRINT_NUMBERS
+#define PRINT_NUMBERS
+
 void print_numbers(void);
+
+#endif
+
+
+#ifndef PRINT_MOST_NUMBERS
+#define PRINT_MOST_NUMBERS
+
 void print_most_numbers(void);
+
+#endif
+
+
+#ifndef MORE_NUMBERS
+#define MORE_NUMBERS
+
 void more_numbers(void);
+
+#endif
+
+
+#ifndef PRINT_LINE
+#define PRINT_LINE
+
 void print_line(int n);
+
+#endif
+
+
+#ifndef PRINT_DIAGONAL
+#define PRINT_DIAGONAL
+
 void print_diagonal(int n);
+
+#endif
+
+
+#ifndef PRINT_SQUARE
+#define PRINT_SQUARE
+
 void print_square(int size);
+
+#endif
+
+
+#ifndef PRINT_TRIANGLE
+#define PRINT_TRIANGLE
+
 void print_triangle(int size);
+
+#endif

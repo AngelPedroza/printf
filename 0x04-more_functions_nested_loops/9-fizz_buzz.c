62b30626dc14e30091ceafeb6861a1f889516e63
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-26 21:46:14 +0000

    fix: final space

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,8 +17,10 @@ int main(void)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && i != 100)
 			printf("Buzz ");
+		else if (i == 100)
+			printf("Buzz");
 		else
 			printf("%d ", i);
 	}

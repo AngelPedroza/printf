9c5a6467ca38019856fdfd1d230b49223bc1cbd8
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-09-26 21:26:08 +0000

    first attempt

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,27 @@
-#include "holberton.h"
+#include <stdio.h>
+
+
+/**
+ * main - fizz buzz iteration
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("Fizz Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	putchar('\n');
+	return (0);
+}

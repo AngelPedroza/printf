7f688fcb1b29bf6a53cd94cb4f2b7b1a400e4432
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-01 20:56:18 +0000

    corrected zero and negative cases

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,13 +11,20 @@
 void print_array(int *a, int n)
 {
 	int i;
+	if (n <= 0)
+		printf("\n");
 
-	for (i = 0; i < n; i++)
+	else
 	{
-		if (i != (n - 1))
-			printf("%d, ", a[i]);
-		else
-			printf("%d\n", a[i]);
+
+		for (i = 0; i < n; i++)
+		{
+			if (i != (n - 1))
+				printf("%d, ", a[i]);
+			else
+				printf("%d\n", a[i]);
+		}
+
 
 	}
 }

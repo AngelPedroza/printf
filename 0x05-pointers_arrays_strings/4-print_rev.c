5ce63d801b7af00114d1dfbc8f345f807b0ba1eb
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-01 14:57:12 +0000

    corrected iteration

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,10 +13,11 @@ void print_rev(char *s)
 	while (*(s + i) != '\0')
 		i++;
 
-	while ((s + i) >= (s + 0))
+	while ((s + i) > (s + 0))
 	{
-		_putchar(*(s + i));
 		i--;
+		_putchar(*(s + i));
+
 	}
 	_putchar('\n');
 

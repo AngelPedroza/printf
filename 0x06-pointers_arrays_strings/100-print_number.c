28ece9d3f4f3622d1f7904bf625210b8bb084719
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-04 21:22:27 +0000

    fix: use of type unsigned

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -8,15 +8,16 @@
 
 void print_number(int n)
 {
+	unsigned int num = n;
 	if (n < 0)
 	{
-		n *= -1;
+		num *= -1;
 		_putchar('-');
 	}
-	if (n / 10)
+	if (num / 10)
 	{
-		print_number(n / 10);
+		print_number(num / 10);
 	}
-		_putchar((n % 10) + '0');
+		_putchar((num % 10) + '0');
 
 }

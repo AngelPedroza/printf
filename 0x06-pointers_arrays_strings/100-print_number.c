8f282bfbbed635af2c9738d170d1f25f3729b53c
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-04 21:13:21 +0000

    task 9

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,42 +1,22 @@
 #include "holberton.h"
 
 /**
+ * print_number - prints an integer
  *
- *
- *
- *
- *
+ * @n: the integer to print
  */
 
 void print_number(int n)
 {
-
-/*	int a;
-	int b = 1;
-
-	for (a = 0; n / b > 9 ; a++)
-	{
-		b = b * 10;
-	}
-	for (; a >= 0; a--)
-	{
-		n = n % b;
-		_putchar(n + '0');
-		b = b / 10;
-	}
-
-*/
-	if(n < 0)
+	if (n < 0)
 	{
 		n *= -1;
 		_putchar('-');
 	}
-        if (n/10)
-        {
-	        print_number(n/10);
-        }
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
 		_putchar((n % 10) + '0');
-
-	
 
 }

103e21eb5488158996f6fc50ba9c538f5eed251f
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-07 19:03:54 +0000

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,22 @@
 #include "holberton.h"
+
+/**
+ * _strchr - returns a pointer to the first ocurrence of char c
+ *
+ * @s: string to look into
+ * @c: the character to look for
+ * Return: pointer to the ffirst ocurrence
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	for ( ; *s; s++)
+	{
+		if (*s == c)
+			break;
+	}
+	if (*s == c)
+		return (s);
+	return (0);
+}

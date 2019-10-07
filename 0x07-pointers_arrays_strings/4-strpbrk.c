027650a0147eb0239a0c04e1b998099fc36ac614
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-07 22:02:31 +0000

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,1 +1,25 @@
 #include "holberton.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ *
+ * @s: the string to evaluate
+ * @accept: accepted characters.
+ * return: pointer to the first ocurrence
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+
+	for (; *s; s++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+				return (s);
+		}
+	}
+
+	return (*s == *accept ? s : 0);
+}

fdae00b496270355debac54be165423313c91ec1
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-07 20:22:03 +0000

    task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,1 +1,22 @@
 #include "holberton.h"
+
+/**
+ * print_diagsums - returns the sum of the diagonal of a matrix
+ *
+ * @a: the name of the array
+ * @size: the size of the array in vertical
+ *
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j, sum;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			sum += a[i][j];
+		}
+	}
+	return (sum);
+}

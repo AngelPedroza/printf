1a2570b0dcd484aafbefd15df35c74b7ceb895cf
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-07 20:18:11 +0000

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,1 +1,29 @@
 #include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, c;
+
+	c = 0;
+	for (i = 0; s[i]; i++)
+		for (j = 0; accept[j]; j++)
+		{
+			if(s[i] == accept[j])
+			{
+				c++;
+				i++;
+			}
+			else
+			{
+				continue;
+			}
+		}
+	return (c);
+}

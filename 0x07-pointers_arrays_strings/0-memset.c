165ef8f8d70ff351dcb8d1bd5c8b03e1c36a274c
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-07 17:10:50 +0000

    tasks 1 and 2

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,20 @@
 #include "holberton.h"
+
+/**
+ * _memset -  fills memory with a constant byte
+ * @s: the
+ * @b: the charater to fill
+ * @n: the number of times to fill
+ * Return: poiter to memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n && s != '\0'; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,21 @@
 #include "holberton.h"
+
+/**
+ * _memcpy - copies memory area
+ *
+ * dest: destination buffer
+ * @src: the source buffer
+ * @n: number of bytes of memory to copy
+ * Return: a pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}

4c558d8fe41e32789a6bb4a502cd98984e0993d6
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-08 00:40:08 +0000

    task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,1 +1,23 @@
 #include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j< 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+
+		_putchar('\n');
+	}
+
+}

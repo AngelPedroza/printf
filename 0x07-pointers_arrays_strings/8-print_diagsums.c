76b1bce0287ec8afa7c52e4661388447d6d630d2
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-08 02:33:35 +0000

    fix: betty format

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - returns the sum of the diagonal of a matrix
@@ -10,13 +11,21 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum;
-	for (i = 0; i < size; i++)
+	unsigned int i, sum1, sum2, tam;
+
+	tam = size;
+	sum1 = 0;
+	sum2 = 0;
+	for (i = 0; i < tam; i++)
 	{
-		for (j = 0; j <= i; j++)
-		{
-			sum += a[i][j];
-		}
+		sum1 += *(a + ((tam + 1) * i));
+
 	}
-	return (sum);
+	printf("%d ", sum1);
+	for (i = 0; i < tam ; i++)
+	{
+		sum2 += *(a + ((tam - 1) * i) + tam - 1);
+	}
+	printf("%d\n", sum2);
+
 }

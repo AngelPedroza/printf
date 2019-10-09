b23270c411c9d8e82c9f2a308593fc0624c6d2b3
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-09 22:18:55 +0000

    fix: betty format

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
- * _sqrtrep - receives the int n as first parameter in order to perform recursio
+ * _sqrtrep - receives the int n as first parameter to perform recursio
  * @a: receives n as input
  * @b: the number to compare by itself to see if it's the sqrt
- * return: the square root | -1 if there are no sq
+ * Return: the square root | -1 if there are no sq
  */
 
 int _sqrtrep(int a, int b)
@@ -22,7 +22,7 @@ int _sqrtrep(int a, int b)
  * _sqrt_recursion - returns the square root of an integer
  *
  * @n: the integer to evaluate
- * return: the square root
+ * Return: the square root
  */
 
 int _sqrt_recursion(int n)

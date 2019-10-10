6d2e5c63e66e7b417231503cca993d9a4c56327f
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-10 20:47:21 +0000

    fix: order of conditional statements

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -18,10 +18,11 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 ==  '*')
 	{
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-			return (1);
 		else if (*s2 == '*' && *s2 + 1 != 0 && *s1 == '\0')
 			return (0);
+
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
 	}
-        return (0);
+	return (0);
 }

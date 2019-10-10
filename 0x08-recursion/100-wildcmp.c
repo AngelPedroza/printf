0c2100461178a4abdc200c3a9b0112b564714c49
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-10 20:56:03 +0000

    fix: error using 0 instead of null char

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -18,7 +18,7 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 ==  '*')
 	{
-		if (*s2 == '*' && *s2 + 1 != 0 && *s1 == '\0')
+		if (*s2 == '*' && *s2 + 1 != '\0' && *s1 == '\0')
 			return (0);
 
 		else if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))

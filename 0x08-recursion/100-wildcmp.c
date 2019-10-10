e60f88c932d7339fcf6e4ca96474b777b5b2aea2
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-10 20:51:30 +0000

    fix error in declaration of conditions

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -18,10 +18,10 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 ==  '*')
 	{
-		else if (*s2 == '*' && *s2 + 1 != 0 && *s1 == '\0')
+		if (*s2 == '*' && *s2 + 1 != 0 && *s1 == '\0')
 			return (0);
 
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+		else if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
 			return (1);
 	}
 	return (0);

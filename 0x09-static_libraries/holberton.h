c964736f15e79a3dc7f6243fb43fcb51ed525e80
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-12 17:42:12 +0000

    fix: definition of library

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,10 +1,7 @@
-#ifndef _PUTCHAR
-#define _PUTCHAR
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 int _putchar(char c);
-
-#endif
-
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
@@ -24,3 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif

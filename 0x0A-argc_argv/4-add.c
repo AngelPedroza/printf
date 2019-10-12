9435c1f7bb4683ff5e145925c2c8e2ce77861ff9
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-12 23:21:36 +0000

    fix : everything

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,33 +12,26 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, j, sum;
 
 	sum = 0;
 
-	if (argc == 1)
+	for (i = 1; i < argc; i++)
 	{
-		printf("0\n");
-		return (1);
-	}
 
-	else
-	{
-		for (i = 1; i < argc; i++)
-
-			if (*argv[i] < 48 || *argv[i] > 57)
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < 47 || argv[i][j] > 57)
 			{
 				printf("Error\n");
 				return (1);
 			}
-			else
-			{
-				sum += atoi(argv[i]);
-			}
-		printf("%d\n", sum);
-		return (0);
+
+		}
+		sum += atoi(argv[i]);
 
 	}
-	return (1);
+
+	printf("%d\n", sum);
 
 }

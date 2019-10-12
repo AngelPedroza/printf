6828d57924663162cea1dbc3f58859cdfb04e47f
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-12 18:01:58 +0000

    fix: logic order

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -18,22 +18,25 @@ int main(int argc, char *argv[])
 
 	int change = atoi(argv[1]);
 
-	if (change < 0)
-		printf("0\n");
 	coins = 0;
 	i = 0;
 
 	if (argc == 2)
 	{
-		while (change > 0)
+		if (change < 0)
+			printf("0\n");
+		else
 		{
-			coins += change / cents[i];
-			change = change % cents[i];
-			if (change % cents[i] != 0)
-				i++;
+			while (change > 0)
+			{
+				coins += change / cents[i];
+				change = change % cents[i];
+				if (change % cents[i] != 0)
+					i++;
+			}
+			printf("%d\n", coins);
+			return (0);
 		}
-		printf("%d\n", coins);
-		return (0);
 	}
 	printf("Error\n");
 	return (1);

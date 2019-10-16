f3a62b10b8c99ef3e3a9df6a168818a8dd978792
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-16 02:44:43 +0000

    fix used strlen instad of for cycle

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,6 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/**
+ * _strlen - returns the lenght of a strin
+ *
+ * @s: the string to evaluate
+ * Return: lenght
+ *
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	i--;
+	return (i);
+}
+
+
 /**
  * _strdup -  returns a pointer to a newly allocated space in memory,
  * which contains a copy of the string given as a parameter.
@@ -22,8 +42,7 @@ char *_strdup(char *str)
 
 	else
 	{
-		for (i = 0; str[i] != '\0'; i++)
-			len++;
+		len = _strlen(str);
 		nstr = malloc(len + sizeof(char) + 1);
 
 		if (nstr == NULL)

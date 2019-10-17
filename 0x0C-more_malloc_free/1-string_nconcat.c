254498b1f7a9960a85c83aaaa42cdaf11b057712
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-17 21:42:58 +0000

    fix conditions for len

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,25 +3,6 @@
 #include <stdio.h>
 
 /**
- * _strlen - returns the length of a string
- *
- * @s: the string to eval
- * Return: length
- */
-/*
-unsigned int _strlen(char *s)
-{
-	unsigned int k;
-
-	k = 0;
-	while (s[k] != 0)
-	{
-		k++;
-	}
-	return (k);
-}
-*/
-/**
  * string_nconcat - concatenates n chars from one string to another
  *
  * @s1: string 1
@@ -53,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
         }
-	for (i = 0; i <= len1 - 1; i++)
+	for (i = 0; i < len1; i++)
 		str[i] = s1[i];
 	for (j = 0; s2[j] != '\0' && j != n ; j++, i++)
 		str[i] = s2[j];

76e85d43ef68eb3b60ce10847ee00d0354885582
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-17 21:32:15 +0000

    fix order of checking of null chars

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,26 +36,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, len1, len2;
 	char *str;
 
-	/*len1 = _strlen(s1) - 1;*/
+	if (s2 == NULL)
+		s2 = "";
+	if (s1 == NULL)
+		s1 = "";
 
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 	{}
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 	{}
-	if (n >= (len2 - 1))
-		n = (len2 - 1);
-
+	if (n >= len2)
+		n = (len2);
 
 	str = malloc(len1 + n + 1);
 	if (str == NULL)
 	{
 		return (NULL);
         }
-	if (s2 == NULL)
-		s2 = "";
-	if (s1 == NULL)
-		s1 = "";
-
 	for (i = 0; i <= len1 - 1; i++)
 		str[i] = s1[i];
 	for (j = 0; j < n; j++, i++)

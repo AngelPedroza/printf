7d6170399f9359184315a56b156955b985f2ba06
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-17 19:15:38 +0000

    fix: error status

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -14,10 +14,11 @@ void *malloc_checked(unsigned int b)
 {
 	unsigned int *ptr;
 
-	ptr = malloc(b * sizeof(unsigned int));
+	ptr = malloc(b);
 	if (ptr == NULL)
 	{
 		exit(98);
 	}
+
 	return (ptr);
 }

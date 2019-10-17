35f5cd956292673fe42bf22d143d7743de522188
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-17 21:38:47 +0000

    fix : ???

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,9 +41,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 
-	for (len1 = 0; s1[len1] != '\0'; len1++)
+	for (len1 = 0; s1[len1]; len1++)
 	{}
-	for (len2 = 0; s2[len2] != '\0'; len2++)
+	for (len2 = 0; s2[len2] && len2 < n; len2++)
 	{}
 	if (n >= len2)
 		n = (len2);
@@ -55,7 +55,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
         }
 	for (i = 0; i <= len1 - 1; i++)
 		str[i] = s1[i];
-	for (j = 0; j < n; j++, i++)
+	for (j = 0; s2[j] != '\0' && j != n ; j++, i++)
 		str[i] = s2[j];
 
 	str[i] = '\0';

887a4c620f0056dd5b3761fc808a6722888a0c42
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-21 19:43:03 +0000

    fix: typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,12 +21,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
-
 
 #endif

c369fb487a3dc5d94db34b1e0987f92324618b0d
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-21 20:33:45 +0000

    task 5 and updated proto in header

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees dogs
+ *
+ * @d: the dog_t typedef structure to free
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}

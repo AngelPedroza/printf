5268712bf16c85315dc7d5825394edf3b9f2e6b9
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-22 00:04:10 +0000

    fix: added denominators to nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 	{}
 
-	d->name == NULL ? printf("(nil)\n") : printf("Name: %s\n", d->name);
-	d->age == 0 ? printf("(nil)\n") : printf("Age: %f\n", d->age);
-	d->owner == NULL ?  printf("(nil)\n") : printf("Owner: %s\n", d->owner);
+	d->name == NULL ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
+	d->age == 0 ? printf("Age: (nil)\n") : printf("Age: %f\n", d->age);
+	d->owner == NULL ?  printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
 }

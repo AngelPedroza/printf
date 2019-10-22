15971d649ce06efd70b8b7687f9c40f2cd39f370
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-22 00:32:04 +0000

    fix: do not do anything if ptr is NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,15 +10,14 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		break;
-	}
-
-	d->name == NULL ? printf("Name: (nil)\n") :
+		d->name == NULL ? printf("Name: (nil)\n") :
 printf("Name: %s\n", d->name);
-	d->age == 0 ? printf("Age: (nil)\n") :
+		d->age == 0 ? printf("Age: (nil)\n") :
 printf("Age: %f\n", d->age);
-	d->owner == NULL ?  printf("Owner: (nil)\n") :
+		d->owner == NULL ?  printf("Owner: (nil)\n") :
 printf("Owner: %s\n", d->owner);
+
+	}
 }

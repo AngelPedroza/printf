f233c9bae1082a290578e1ebbb598b987b53c8f7
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-22 00:01:43 +0000

    fix: added newline to nil print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,10 +13,7 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 	{}
 
-	d->name == NULL ? printf("(nil)") : printf("Name: %s\n", d->name);
-	if (d->age == 0)
-		printf("(nil)");
-	else
-		printf("Age: %f\n", d->age);
-	d->owner == NULL ?  printf("(nil)") : printf("Owner: %s\n", d->owner);
+	d->name == NULL ? printf("(nil)\n") : printf("Name: %s\n", d->name);
+	d->age == 0 ? printf("(nil)\n") : printf("Age: %f\n", d->age);
+	d->owner == NULL ?  printf("(nil)\n") : printf("Owner: %s\n", d->owner);
 }

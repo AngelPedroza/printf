aa88b8f781bc4c2f5e90c770c99336f354b2f34b
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-22 00:44:01 +0000

    fix: only exec if ptr != NULL

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,10 +13,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return (NULL);
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
 }

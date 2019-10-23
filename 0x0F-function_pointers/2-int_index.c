5a3a96a7b814116bf815c40c1003c5efdb8eab92
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-23 18:47:27 +0000

    [feature]: added task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index returns the position in which a match is found i.e. return is 1
+ *
+ * @array: array to look into
+ * @size: the size of the array
+ * @cmp: the function to use to discriminate
+ * Return: position in i, -1
+ */
+
+
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, index;
+	if (size < 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		index = cmp(array[i]);
+		if (index == 1)
+	        return (i);
+
+	}
+	return (-1);
+}

be9c62de032af4a10d374577d5ed29b4e3c90b14
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-23 18:18:43 +0000

    [feature]: added tasks 0 and 1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * -print_name - prints the name accorrding to the function invoked as 2nd arg
+ *
+ * @name: the name to print
+ * @f: pointer to the function to use. 
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+	unsigned int i;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}

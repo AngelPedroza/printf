45a67d54906bdf0197b25b474727886594b3f856
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-24 00:37:31 +0000

    fix: logic for symbols

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+#include <string.h>
 
 /**
  * main - outputs the result of calculation program
@@ -15,15 +16,16 @@ int main(int argc, char *argv[])
 
 	if (argc == 4)
 	{
+		res1 = atoi(argv[1]);
+		res2 = atoi(argv[3]);
 
-		if (*argv[1] >= '0' && *argv[1] <= '9')
-		{
-			res1 = atoi(argv[1]);
-		}
 
-		if (*argv[3] >= '0' && *argv[3] <= '9')
+		if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
+		     && argv[2][0] != '/' && argv[2][0] != '%')
+		    || strlen(argv[2]) != 1)
 		{
-			res2 = atoi(argv[3]);
+			printf("Error\n");
+			exit(99);
 		}
 		res = (*get_op_func(argv[2]))(res1, res2);
 
@@ -31,6 +33,7 @@ int main(int argc, char *argv[])
 		return (0);
 
 	}
+
 
 	printf("Error\n");
 	exit(98);

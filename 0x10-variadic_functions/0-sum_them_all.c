97ab5118f514e12b1728123ce6863ed225db053b
Nukemenonai <david.giovanni.ovalle@gmail.com>
2019-10-25 14:43:06 +0000

    [Feature]: Add task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,2 +1,25 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
+/**
+ * sum_them_all - sums all its arguments
+ *
+ * @n: the variadic number of arguments it contains
+ * Return: result of the sum
+ *
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i, sum;
+
+	va_start(valist, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (sum);
+}

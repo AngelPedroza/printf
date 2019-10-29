a2d9c4a9fbd95ca43605c526694d4646acc779f7
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-29 02:16:33 +0000

    [Feature] Try to solve the problem creating 2 iterators that print me n the buffer and move on the format

--- holberton.h
+++ holberton.h
@@ -1,13 +1,26 @@
-#ifndef HOLBERTON
-#define HOLBERTON
-#include <unistd.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
-typedef struct specify {
-	char *c;
-	void (*f)(va_list list);
-}specify;
+typedef struct specifier
+{
+	char *sp;
+	int (*f)();
+} myforspec;
+/*Auxiliar functions*/
+int _strlen(char *str);
+int _printf(char *format,...);
+void moveinto_buffer(char *buffer, char c, int position);
 
-int _printf(const char *format, ...);
 
+/*Operations of my structure*/
+int _printchar(va_list valist, char *buffer, int i);
+int printint(va_list valist, char *buffer, int i);
+int printstr(va_list valist, char *buffer, int i);
+int printhexa(va_list valist, char *buffer, int i);
+int printoctal(va_list valist, char *buffer, int i);
+int printbin(va_list valist, char *buffer, int i);
+char *convert(unsigned int num, int base);
 #endif

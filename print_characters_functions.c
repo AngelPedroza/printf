3cd966e03ab97c9fcc5ffac8c0e890457dc7aca6
Angel Pedroza <55103543+AngelPedroza@users.noreply.github.com>
2019-10-29 19:29:41 -0500

    [Feature]: Add new function to capitalize letters

--- print_characters_functions.c
+++ print_characters_functions.c
@@ -54,3 +54,38 @@ int PPS(char *buffer, int *count)
 	moveinto_buffer(buffer, '%', count);
 	return (0);
 }
+/**
+ * capitalize - capitalizes a string
+ *
+ * @valist: the argument list to take the string from
+ * @buffer: My buffer is a malloc declaration that I will use for print all.
+ * @count: This is the parameter to count the position that should put
+ * in the buffer.
+ * Return: Always is 0.
+ */
+int capitalize(va_list valist, char *buffer, int *count)
+{
+       int it, i, j, len;
+       char *s, *d;
+       char upperalpha[] = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+       char loweralpha[] = "abcdefghijklmnñopqrstuvwxyz";
+       s = va_arg(valist, char *);
+       len = _strlen(s);
+       d = malloc(len);
+       for (j = 0; s[j] != '\0'; j++)
+       {
+               for (i = 0; loweralpha[i] != '\0'; i++)
+               {
+                       if (s[j] == loweralpha[i])
+                               d[j] = upperalpha[i];
+                       if (s[j] == upperalpha[i])
+                               d[j] = upperalpha[i];
+               }
+       }
+       for (it = 0; it < len; it++)
+       {
+               moveinto_buffer(buffer, d[it], count);
+       }
+       free(d);
+       return (0);
+}
